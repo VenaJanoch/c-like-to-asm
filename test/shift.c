94716613f5105e909dc12cb14c7b7752342d686b
Vasek <vena.janoch@windowslive.com>
2017-11-28 10:33:53 +0100

    Added shift test

---
+++ test/shift.c
@@ -0,0 +1,37 @@
+uint8 Main(){
+
+/*	PrintString("Zadejte cislo pro posunuti");
+	PrintNewLine();
+
+	uint16 number32 = ReadUint32();
+	*/
+	
+	uint16 number16;
+	number16 = 60;
+	PrintNewLine();
+
+	uint16 i;
+	uint32 shiftNumber;
+
+	for (i = 1; i < 9; ++i) {
+
+		shiftNumber = number16 >> i;
+		PrintString("Cislo je posunute o ");
+		PrintUint32(i);
+		PrintString(" bitu do prava >> ");
+		PrintUint32(shiftNumber);
+		PrintNewLine();
+
+		shiftNumber = number16 << i;
+		PrintString("Cislo je posunute o ");
+		PrintUint32(i);
+		PrintString(" bitu do leva << ");
+		PrintUint32(shiftNumber);
+		PrintNewLine();
+
+	}
+
+
+
+	return 0;
+}
