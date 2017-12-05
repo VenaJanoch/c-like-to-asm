79a38c41bec1e0d90f5edd412b362511c5482718
Vasek <vena.janoch@windowslive.com>
2017-12-05 19:01:04 +0100

    Updated pointers test

--- test/pointers.c
+++ test/pointers.c
@@ -1,6 +1,6 @@
 
 uint32 setrid_pole(uint32 velikost, uint32* pole);
-uint32 zvet_pole(uint32 opakovani, uint32 mez, uint32* pole);
+uint32 zvets_pole(uint32 opakovani, uint32 mez, uint32* pole);
 uint32 vytiskni_pole(uint32 velikost, uint32* pole);
 uint32 prekopiruj_pole(uint32* zdroj, uint32* cil, uint32 velikost);
 
@@ -11,33 +11,39 @@ uint8 Main(){
 		uint32 mez = 10;
 		uint32 opakovani = 1;
 		uint32* pole = alloc<uint32>(mez);
-	
+		uint32 hrana;
+
 		while (true == true) {
 
 			PrintString("Zadejte cislo z intervalu <0,1000> pro serazeni \r\n");
 			PrintString("Pro ukonceni zadavani zadejte cislo vetsi nez 1000 \r\n");
 
 			cislo = ReadUint32();
-
+			PrintNewLine();
+			
 			if (cislo > 1000) {				
 				break;
-			}
-
-			if (counter == (mez * opakovani)) {
-				zvets_pole(opakovani, mez, pole);
+			}		
 			
+			hrana = ((mez - 1) * opakovani);
+
+			if (counter == hrana) {			
 				++opakovani;
+				zvets_pole(opakovani, mez, pole);
 			}
 
 			pole[counter] = cast<uint32>(cislo);
-			
 			++counter;
 	}
-		--counter;
-		setrid_pole(counter, pole);
 		
-		vytiskni_pole(pole);
-		
+		PrintString("------------------------------------ \r \n");
+		PrintString("Pocet prvku: ");
+		PrintUint32(counter);
+		PrintNewLine();
+
+		setrid_pole(counter, pole);		
+		vytiskni_pole(counter, pole);
+		release(pole);
 
 	return 0;
 }
@@ -53,7 +59,7 @@ uint32 setrid_pole(uint32 velikost, uint32* pole) {
 
 		return 0;
 	}
-
+	
 	for (j = 0; j < velikost - 1; ++j) {
 		i = 0;
 		do {
@@ -75,6 +81,8 @@ uint32 vytiskni_pole(uint32 velikost, uint32* pole) {
 		PrintUint32(pole[i]);
 		PrintString(", ");
 	}
+
+	PrintNewLine();
 	return 0;
 }
 
@@ -96,13 +104,13 @@ uint32 zvets_pole(uint32 opakovani, uint32 mez, uint32* pole) {
 
 	uint32* pom_pole = alloc<uint32>(pom_velikost);
 	
-	prekopiruj_pole(pole, pom_pole, mez);
+	prekopiruj_pole(pole, pom_pole, pom_velikost);
 
 	release(pole);
 
 	pole = alloc<uint32>(mez * opakovani);
 
-	prekopiruj_pole(pom_pole, pole, mez);
+	prekopiruj_pole(pom_pole, pole, pom_velikost);
 	
 	return 0;
 }
